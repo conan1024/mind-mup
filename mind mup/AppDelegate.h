1ab17e1679332d8c0ae0eec1e8999ce4e80c3de7
Masanari Miyamoto <masanarimiyamoto@Masanari-no-MacBook-Air.local>
2014-06-15 09:55:24 +0900

    Initial Commit

---
+++ mind mup/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  mind mup
+//
+//  Created by Masanari Miyamoto on 2014/06/15.
+//  Copyright (c) 2014年 Masanari Miyamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ mind mup/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  mind mup
+//
+//  Created by Masanari Miyamoto on 2014/06/15.
+//  Copyright (c) 2014年 Masanari Miyamoto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
